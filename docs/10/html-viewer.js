5ecbbb195e5c4f596401772bf1b9c6d1ab2be5f5
ytyaru <yttry0@gmail.com>
2023-12-05 19:32:36 +0900

    10 パフォーマンス改善途中２（複数ブロックをまたぐカットに対応）

---
+++ docs/10/html-viewer.js
@@ -0,0 +1,56 @@
+(function(){
+const { div, span, h1, p, br } = van.tags
+class HtmlViewer {
+    constructor() {
+        this.parser = new JavelParser()
+        this._id = 'html-viewer'
+        this.ja = van.state('# 原稿\n\n　これは原稿です。自由に書いてください。\n\n　構文は３つです。見出し、パラグラフ、改行です。\n　見出しは行頭に#と半角スペースのあとに文字を書きます。\n　パラグラフは空行を挟みます。\n　改行は連続したテキスト間にひとつだけ改行を入れるとパラグラフ内で改行できます。\n\n　このテキストエリアに原稿を書くと、↓にHTMLとして表示されます。\n\n　↓のボタンを押すと横書き／縦書きを切替します。\n\n　HTML表示領域は長くなるとスクロールします。縦書きのときは横スクロールですが、マウスホイールでスクロールできます。\n\n# ｜見出し《heading》で《《強調》》と送《おく》り仮名《がな》\n\n　拡張構文としてemとrubyを実装しました。｜《｜《強調｜》｜》のようにすると傍点（圏点）がつきます。また、漢字｜《かんじ｜》のように書くとルビが振れます。もし漢字以外の文字が含まれている箇所にルビを振りたいときは最初の文字にハイフンをつけます。｜あいうえお《アイウエオ》のようになります。')
+        //this._htmls = van.derive(this.#jaToHtmls.bind(this))
+        //this._htmls = van.derive(()=>this.parser.toHtmls(this.ja.val))
+        this._htmls = van.state([])
+        //this._htmls = van.state('')
+        //this._htmls = []
+        this.writingMode = van.state('vertical-rl')
+        console.log(this.writingMode.val)
+        this.textOrientation = van.state('upright')
+        this.overflowX = van.state('auto')
+        this.overflowY = van.state('hidden')
+    }
+    get element() { return div({id:this._id, tabindex:0, 
+            style:this.#style.bind(this),
+            onwheel:(e)=>this.#onWheel(e),
+            onkeydown:(e)=>this.#onKeydown(e),
+//        }, ()=>div(()=>this._htmls.val.map(h=>h.outerHTML)))
+        }, ()=>div(this._htmls.val))
+//        }, ()=>div(this._htmls))
+    }
+    toggleWritingMode() {
+        this.writingMode.val= (this.isVertical) ? 'horizontal-tb' : 'vertical-rl'
+        this.overflowX.val = (this.isVertical) ? 'auto' : 'hidden'
+        this.overflowY.val = (this.isVertical) ? 'hidden' : 'auto'
+        this.textOrientation.val = (this.isVertical) ? 'upright' : 'mixed'
+    }
+    get isVertical() { return ('vertical-rl'===this.writingMode.val) }
+    get isHorizontal() { return ('horizontal-tb'===this.writingMode.val) }
+    #onWheel(e) {
+        if ('vertical-rl'===this.writingMode.val) {
+            if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
+            const target = document.querySelector('#html-viewer')
+            target.scrollLeft += e.deltaY;
+            e.preventDefault();
+        }
+    }
+    #onKeydown(e) {
+        if (['Right','Left','Up','Down'].some((key)=>`Arrow${key}`===e.code)) {
+            if ('ArrowUp'===e.code) e.target.scrollTop -= 96
+            if ('ArrowDown'===e.code) e.target.scrollTop += 96
+            if ('ArrowLeft'===e.code) e.target.scrollLeft -= 96
+            if ('ArrowRight'===e.code) e.target.scrollLeft += 96
+            e.preventDefault()
+        }
+    }
+    // html-viewerは縦書きでHTML表示したいからdiv要素にする。でもdiv要素はfocusが当たらない。なのでtabindex=0を設定した。標準のキー操作だと矢印の上を押し続けるとbody要素へフォーカスが飛んでしまう。なのでキャレットを排除すべくuser-select:none;にして、かつキーイベントでスクロール操作するよう実装した。
+    #style() { console.log(this.writingMode.val);return `writing-mode:${this.writingMode.val};text-orientation:${this.textOrientation.val};box-sizing:border-box;overflow-x:${this.overflowX.val};overflow-y:${this.overflowY.val};user-select:none;` }
+}
+window.HtmlViewer = HtmlViewer
+})()
---
+++ docs/10/text-block.js
@@ -0,0 +1,33 @@
+(function(){
+class TextBlock {
+    static fromText(text) { return this.fromLines(this.#textToLines(text)) }
+    static fromLines(lines) {
+        const [blocks, block] = [[], []]
+        for (let line of lines) {
+            block.push(line)
+            if (''===line && 0 < block.length) { blocks.push(block.join('\n')); block.splice(0); }
+        }
+        if (0 < block.length) { blocks.push(block.join('\n')) }
+        return blocks.filter(v=>v)
+    }
+    static #textToLines(text) { return text.trim().split(/\r?\n/) }
+    static selectedIndex(selectionEnd, text) { return this.selectedIndexBlock(selectionEnd, text)[0] } 
+    static selectedText(selectionEnd, text) { return this.selectedIndexBlock(selectionEnd, text)[1] } 
+    static selected(selectionEnd, text) {
+        // テキストブロック・インデックス取得
+        const textBefore = text.slice(0, selectionEnd) // キャレット位置より前
+        const textAfter = text.slice(selectionEnd) // キャレット位置より後
+        //const selectedBlockIndex = (textBefore.match(/[\n]{2,}/g) || []).length
+        const selectedBlockIndex = this.count(textBefore)-1
+        console.log('selectedBlockIndex:', selectedBlockIndex )
+        // キャレット位置のブロックテキスト取得
+        const blockStart = textBefore.lastIndexOf('\n\n')
+        const blockEnd = textAfter.indexOf('\n\n')
+        console.log(blockStart, blockEnd+textBefore.length)
+        const block = text.slice(((-1<blockStart) ? blockStart+2 : 0), (-1<blockEnd) ? blockEnd+textBefore.length : text.length)
+        return [selectedBlockIndex, block]
+    }
+    static count(text) { return (text.match(/[\n]{2,}/g) || []).length + 1 } // text内にあるブロック数
+}
+window.TextBlock = TextBlock
+})()
