abda09227686f2b7df205c508f0381593a82c408
ytyaru <yttry0@gmail.com>
2023-12-13 13:20:38 +0900

    add 19

---
+++ docs/19/text-input.js
@@ -0,0 +1,82 @@
+(function(){
+const { textarea } = van.tags
+class TextInput {
+    constructor(htmlViewer) { this.id='manuscript'; this.htmlViewer=htmlViewer; this.isComposing=false; this.isCut=false; this.isPaste=false; }
+    get element() { return textarea({id:this.id, placeholder:'原稿', style:()=>`box-sizing:border-box;`,
+        oninput:(e)=>this.#onInput(e),
+        oncut:(e)=>this.#onCut(e),
+        onpaste:(e)=>this.#onPaste(e),
+        oncompositionend:(e)=>this.htmlViewer.ja.val = e.target.value},
+        this.htmlViewer.ja.val)
+    }
+    #onInput(e) {
+        console.log('INPUT !!!!!!!!!')
+        if (this.#isComposing(e)) { return }
+        if (this.isCut) { this.isCut=false; return; }
+        if (this.isPaste) { this.isCut=false; return; }
+        console.log(e)
+        this.#parseBlock(e)
+    }
+    #onCut(e) {
+        console.log('CUT !!!!!!!', e)
+        console.log(e.target.selectionStart, e.target.selectionEnd)
+        const [index, blocks, deleteCount] = TextBlock.cutBlocks(e.target.selectionStart, e.target.selectionEnd, e.target.value.trim(), this.htmlViewer.parser.textBlocks)
+        console.log(index, deleteCount, blocks)
+        //this.htmlViewer._htmls.val = [...this.htmlViewer.parser.pasteBlocks(index, blocks, deleteCount)]
+        this.htmlViewer.htmls = this.htmlViewer.parser.pasteBlocks(index, blocks, deleteCount)
+        this.isCut = true
+    }
+    #getClipboardText(e) {
+        if (e.clipboardData && e.clipboardData.getData) { return e.clipboardData.getData('text/plain') } // 他
+        else if (window.clipboardData && window.clipboardData.getData) { return window.clipboardData.getData('Text') } // IE
+        throw new Error('clipboardData API がありません。')
+    }
+    #onPaste(e) {
+        console.log('PASTE !!!!!!!!!')
+        console.log(e.target.selectionStart, e.target.selectionEnd)
+        //const [index, blocks] = TextBlock.pasteBlocks(e.target.selectionStart, e.target.selectionEnd, this.#getClipboardText(e), e.target.value.trim(), this.htmlViewer.parser.textBlocks)
+        const [index, blocks] = TextBlock.pasteBlocks(e.target.selectionStart, e.target.selectionEnd, this.#getClipboardText(e), e.target.value, this.htmlViewer.parser.textBlocks)
+        //this.htmlViewer._htmls.val = [...this.htmlViewer.parser.pasteBlocks(index, blocks)] // 反応させるには新しい別の配列オブジェクトにする必要があるみたい。VanJSの仕様
+        this.htmlViewer.htmls = this.htmlViewer.parser.pasteBlocks(index, blocks)
+        this.isPaste = true
+    }
+    // IME入力中判定 https://qiita.com/alt_yamamoto/items/8663d047a3794dd5605e
+    #isComposing(e) {
+        if (e.isComposing || e.key === 'Process' || e.keyCode === 229) { return true } // IME入力中
+        if (['insertCompositionText', 'deleteCompositionText', 'insertFromComposition', 'deleteByComposition'].includes(e.inputType)) { return true }
+        return false
+    }
+    #composition(e) {
+        if ('compositionstart'===e.type) this.isComposing = true;
+        if ('compositionend'===e.type) this.isComposing = false;
+        if (window.isComposing === true) return; // IME入力途中なので何もしない
+        if (event.type === 'compositionend') {
+            console.log(event.target.value)
+        }
+    }
+    #compositionEnd(e) { this.#parseBlock(e) }
+    #parseBlock(e) {
+        console.log(window.getSelection())
+        console.log(window.getSelection().anchorNode)
+        console.log(window.getSelection().getRangeAt(0).anchorNode)
+        console.log(e.target)
+//        console.log(e.target.getSelection())
+//        console.log(e.target.getSelection().anchorNode)
+//        console.log(e.target.getSelection().getRangeAt(0).anchorNode)
+        console.log(e.target.selectionStart)
+        console.log(e.target.selectionEnd)
+
+        const [index, block] = TextBlock.selected(e.target.selectionStart, e.target.selectionEnd, e.target.value.trim())
+        this.htmlViewer._htmls.val = [...this.htmlViewer.parser.setBlockText(index, block)] // 反応させるには新しい別の配列オブジェクトにする必要があるみたい。VanJSの仕様
+
+        // 範囲選択中に削除・入力したらブロック削除を考慮すべき
+    }
+    setup() {
+        this.htmlViewer._htmls.val = this.htmlViewer.parser.toHtmls(document.querySelector(`#${this.id}`).value)
+        document.querySelector(`#${this.id}`).addEventListener('compositionend', this.#compositionEnd.bind(this))
+        //document.querySelector(`#${this.id}`).addEventListener('compositionstart', this.#composition.bind(this))
+        //document.querySelector(`#${this.id}`).addEventListener('compositionend', this.#composition.bind(this))
+    }
+}
+window.TextInput = TextInput
+})()
