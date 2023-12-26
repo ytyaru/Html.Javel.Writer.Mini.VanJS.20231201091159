df2e432519e091f0c3db629e2b50e55cbf192db8
ytyaru <yttry0@gmail.com>
2023-12-26 17:29:18 +0900

    add 31

---
+++ docs/31/javel-syntax-error.js
@@ -0,0 +1,20 @@
+(function(){
+class JavelSyntaxError {
+    constructor() {
+    }
+    check(text) {
+        const errors = {}
+    }
+}
+class ErrorData {
+    constructor(line, start, end, summary, details, method) {
+        this.line = line
+        this.start = start
+        this.end = end
+        this.summary = summary
+        this.details = details
+        this.method = method
+    }
+}
+window.ErrorViewer = ErrorViewer
+})()
