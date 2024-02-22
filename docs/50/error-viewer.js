2b629817f5016943e04011f541646143b449135c
ytyaru <yttry0@gmail.com>
2024-02-22 21:02:18 +0900

    add 50

---
+++ docs/50/error-viewer.js
@@ -0,0 +1,131 @@
+(function(){
+const { div, span, h1, p, a, br, button, table, tr, th, td, details, summary } = van.tags
+class ErrorViewer {
+    constructor(htmlViewer) {
+        this.htmlViewer = htmlViewer
+        this.parser = new JavelParser()
+        this._id = 'error-viewer'
+        this._htmls = van.state(['Nつのエラーがあります。すべて修正するまでプレビューできません。'])
+        this.display = van.state('none')
+        this.writingMode = van.state('vertical-rl')
+        console.log(this.writingMode.val)
+        this.textOrientation = van.state('upright')
+        this.overflowX = van.state('auto')
+        this.overflowY = van.state('hidden')
+    }
+    get isShow() { return 'block'===this.display.val }
+    set isShow(v) { this.display.val = (v) ? 'block' : 'none' }
+    show() { this.display.val = 'block' }
+    hide() { this.display.val = 'none' }
+    get htmls() { return this._htmls }
+    set htmls(v) { if (Type.isArray(v)) { this._htmls.val = [...v] } }// 反応させるには新しい配列にする必要ある。VanJSの仕様
+    make() { return div({id:this._id, tabindex:0, 
+            style:this.#style.bind(this),
+            onwheel:(e)=>this.#onWheel(e),
+            onkeydown:(e)=>this.#onKeydown(e),
+        }, ()=>div(this._htmls.val))
+    }
+    get element() { return document.querySelector(`#${this._id}`) }
+    toggleWritingMode() {
+        this.writingMode.val= (this.isVertical) ? 'horizontal-tb' : 'vertical-rl'
+        this.overflowX.val = (this.isVertical) ? 'auto' : 'hidden'
+        this.overflowY.val = (this.isVertical) ? 'hidden' : 'auto'
+        this.textOrientation.val = (this.isVertical) ? 'upright' : 'mixed'
+    }
+    get isVertical() { return ('vertical-rl'===this.writingMode.val) }
+    get isHorizontal() { return ('horizontal-tb'===this.writingMode.val) }
+    #onWheel(e) {
+        if ('vertical-rl'===this.writingMode.val) {
+            if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
+            const target = document.querySelector('#html-viewer')
+            target.scrollLeft += e.deltaY;
+            e.preventDefault();
+        }
+    }
+    #onKeydown(e) {
+        if (['Right','Left','Up','Down'].some((key)=>`Arrow${key}`===e.code)) {
+            if ('ArrowUp'===e.code) e.target.scrollTop -= 96
+            if ('ArrowDown'===e.code) e.target.scrollTop += 96
+            if ('ArrowLeft'===e.code) e.target.scrollLeft -= 96
+            if ('ArrowRight'===e.code) e.target.scrollLeft += 96
+            e.preventDefault()
+        }
+    }
+    // html-viewerは縦書きでHTML表示したいからdiv要素にする。でもdiv要素はfocusが当たらない。なのでtabindex=0を設定した。標準のキー操作だと矢印の上を押し続けるとbody要素へフォーカスが飛んでしまう。なのでキャレットを排除すべくuser-select:none;にして、かつキーイベントでスクロール操作するよう実装した。
+    #style() { console.log(this.writingMode.val);return `display:${this.display.val};writing-mode:${this.writingMode.val};text-orientation:${this.textOrientation.val};box-sizing:border-box;overflow-x:${this.overflowX.val};overflow-y:${this.overflowY.val};user-select:none;` }
+    makeHtml(textarea, errors) {
+        this.htmls = [
+            this.#makeSummary(errors),
+            this.#makeAllFixButton(errors, textarea),
+            this.#makeErrorTable(errors, textarea),
+        ]
+    }
+    #makeSummary(errors) { return p(`${errors.length}つのエラーがあります。すべて修正するまでプレビューできません。`) }
+    #makeAllFixButton(errors, textarea) {
+        const hasMethodCount = errors.filter(e=>this.#getMethodNames(e).includes('method')).length
+        if (0===hasMethodCount) { return '' }
+        const text = (hasMethodCount===errors.length) ? '全件自動修正する' : `できるだけ自動修正する　${hasMethodCount}/${errors.length}`
+        return a({href:'javascript:void(0);', onclick:()=>{
+            for (let i=0; i<errors.length; i++) {
+                const diff = this.#fixError(textarea, errors[i], false)
+                this.#resetErrorStart(errors, i, diff)
+            }
+            this.#updateErrors(textarea)
+        }}, text)
+    }
+    #resetErrorStart(errors, idx, diff) { for (let i=idx; i<errors.length; i++) { errors[i].start += diff } }
+    #getMethodNames(obj) {
+        const getOwnMethods = (obj) => Object.entries(Object.getOwnPropertyDescriptors(obj))
+            .filter(([name, {value}]) => typeof value === 'function' && name !== 'constructor')
+            .map(([name]) => name)
+        const _getMethods = (o, methods) => o === Object.prototype ? methods : _getMethods(Object.getPrototypeOf(o), methods.concat(getOwnMethods(o)))
+        return _getMethods(obj, [])
+    }
+    #makeErrorTable(errors, textarea) {
+        return table(
+            tr(th('箇所'), th('内容'), th('修正案')),
+            this.#makeErrorTrs(errors, textarea).flat(),
+        )
+    }
+    #makeErrorTrs(errors, textarea) {
+        const trs = []
+        for (let e of errors) {
+            const line = this.#getLineCount(textarea.value.slice(0, e.start))
+            trs.push(tr(
+                td(a({href:'javascript:void(0);', onclick:()=>this.#selectError(textarea, e)}, line)), 
+                td(details(summary(e.constructor.summary), e.constructor.details)), 
+                td(a({href:'javascript:void(0);', onclick:()=>this.#fixError(textarea, e)}, e.constructor.methodSummary))))
+        }
+        return trs
+    }
+    #getLineCount(text) { return (text.match(/\n/g) || []).length + 1; }
+    #selectError(textarea, error) { textarea.setSelectionRange(error.start, error.end); textarea.focus(); }
+    #fixError(textarea, error, isUpdate=true) {
+        console.log('#fixError():', error)
+        this.#selectError(textarea, error)
+        const afterText = error.method()
+        const beforeLen = error.end - error.start
+        const afterLen = afterText.length
+        const diff = afterLen - beforeLen
+        textarea.setRangeText(afterText, error.start, error.end)
+        console.log('自動修正しました！')
+        if (isUpdate) { this.#updateErrors(textarea) }
+        return diff
+    }
+    #updateErrors(textarea) {
+        console.log('#updateErrors()')
+        const errors = JavelSyntaxError.check(textarea.value)
+        console.log(errors)
+        this.makeHtml(textarea, errors)
+        this.isShow = (0 < errors.length)
+        this.htmlViewer.isShow = !this.isShow
+        console.log(this.isShow, this.htmlViewer.isShow)
+    }
+    checkError(errors) {
+        while ((m = text.exec(/[\n]{3,}/g)) != null) {
+
+        }
+    }
+}
+window.ErrorViewer = ErrorViewer
+})()
---
+++ docs/50/text-block.js
@@ -0,0 +1,127 @@
+(function(){
+class TextBlock {
+    static fromText(text) { return this.fromLines(this.#textToLines(text)) }
+    static fromLines(lines) {
+        const [blocks, block] = [[], []]
+        for (let line of lines) {
+            block.push(line)
+            if (''===line && 0 < block.length) { blocks.push(block.join('\n').trimLine()); block.splice(0); }
+        }
+        if (0 < block.length) { blocks.push(block.join('\n').trimLine()) }
+        return blocks.filter(v=>v)
+    }
+    static #textToLines(text) { return text.trimLine().split(/\r?\n/) }
+    static *iter(text) {
+        const [blocks, block] = [[], []]
+        for (let line of this.#textToLines(text)) {
+            block.push(line)
+            if (''===line && 0 < block.length) { yield block.join('\n').trimLine(); block.splice(0); }
+        }
+        if (0 < block.length) { yield block.join('\n').trimLine(); block.splice(0); }
+    }
+    static selectedIndex(selectionEnd, text) { return this.selectedIndexBlock(selectionEnd, text)[0] } 
+    static selectedText(selectionEnd, text) { return this.selectedIndexBlock(selectionEnd, text)[1] } 
+    static selected(selectionStart, selectionEnd, text) {
+        if (!text.includes('\n\n')) { return [0, text] }
+        // テキストブロック・インデックス取得
+        const textFront = text.slice(0, selectionStart)
+        if (!textFront.includes('\n\n')) { return [0, text.slice(0, text.indexOf('\n\n'))] }
+        const textBack = text.slice(selectionEnd)
+        const selectedBlockIndex = this.count(textFront)-1
+        // キャレット位置のブロックテキスト取得
+        const blockStart = textFront.lastIndexOf('\n\n')
+        const blockEnd = textBack.indexOf('\n\n')
+        console.debug(blockStart, blockEnd+textFront.length)
+        console.debug('textFront:', textFront)
+        console.debug('textBack:', textBack)
+        if (-1 === blockStart) { return [0, textFront] }
+        if (-1 === blockEnd) { return [this.count(text)-1, textBack] }
+        const block = text.slice((blockStart+2), blockEnd+textFront.length)
+        console.debug(selectedBlockIndex, block)
+        return [selectedBlockIndex, block]
+    }
+    static count(text) { return (text.match(/[\n]{2,}/g) || []).length + 1 } // text内にあるブロック数
+    static cutBlocks(selectionStart, selectionEnd, text, blocks) {
+        console.log('cutBlocks():', selectionStart, selectionEnd, text, blocks)
+        const cutText = text.slice(selectionStart, selectionEnd)
+        const cutInnerBlockCount = (cutText.trimLine().match(/[\n]{2,}/g) || []).length
+        const textFront = text.slice(0, selectionStart)
+        const textBack = text.slice(selectionEnd)
+        const blockFront = textFront.slice((-1===textFront.lastIndexOf('\n\n')) ? 0 : textFront.lastIndexOf('\n\n')+2)
+        const blockBack = textBack.slice(0, (-1===textBack.indexOf('\n\n')) ? textBack.length : textBack.indexOf('\n\n'))
+        const [index, block] = this.selected(selectionStart, selectionEnd, text)
+        const deleteCount = cutInnerBlockCount + 1 + this.#deleteCount(cutText, textFront, textBack)
+        const nextBlockText = (index<blocks.length) ? blocks[index+1] : ''
+        const blockEndText = this.#blockBack(cutText, textFront, textBack, blockBack, nextBlockText)
+        const pasteBlocks = this.fromLines((blockFront + blockEndText).split(/\r?\n/))
+        console.debug('textFront:', textFront)
+        console.debug('textBack:', textBack)
+        console.debug('blockFront:', blockFront)
+        console.debug('blockBack:', blockBack)
+        console.debug('blockBack.trimLine():', blockBack.trimLine())
+        console.debug('blockEndText:', blockEndText)
+        console.debug(`cutText: ${(cutText.match(/^[\n]{2,}/g) || []).length} : ${(cutText.match(/[\n]{2,}$/g) || []).length} : ${cutText}`)
+        console.debug('#deleteCount():', this.#deleteCount(cutText, textFront, textBack))
+        console.debug(`cutInnerBlockCount: ${cutInnerBlockCount}`)
+        console.debug(`cutText.trimLine(): ${cutText.trimLine()}`)
+        console.debug(`(cutText.match(/[\n]/g) | []).length: ${(cutText.match(/[\n]/g) || []).length}`)
+        console.debug(`(cutText.match(/[\n]/g) | []).length: ${(cutText.match(/[\n]/g) || []).length}`)
+        console.debug(`((cutText.match(/[\n]/g) | []).length)/2: ${((cutText.match(/[\n]/g) || []).length)/2}`)
+        console.debug(`Math.ceil(((cutText.match(/[\n]/g) | []).length)/2): ${Math.ceil(((cutText.match(/[\n]/g) || []).length)/2)}`)
+        console.debug('blockEndText:', blockEndText)
+        console.debug('blockBack.trimLine()==nextBlockText:', blockBack.trimLine()==nextBlockText)
+        console.debug(index, deleteCount, pasteBlocks)
+        return [index, pasteBlocks, deleteCount]
+    }
+    static #blockBack(cutText, textFront, textBack, blockBack, nextBlockText) {
+        if (cutText.startsWith('\n') && cutText.endsWith('\n') && textFront.endsWith('\n') && textBack.startsWith('\n')) { return '' } // cutText先頭末尾\n含むとき''を返したい
+        if (cutText.endsWith('\n') && textBack.startsWith('\n')) { return blockBack } // cutText先頭末尾\n含むとき''を返したい
+        if (cutText.startsWith('\n') && textFront.endsWith('\n')) { return blockBack }
+        if (cutText.endsWith('\n\n')) { return blockBack }
+        if (blockBack.trimLine()==nextBlockText) { return '' }
+        return blockBack
+    }
+    static #deleteCount(cutText, textFront, textBack) {
+        if ((4 <= cutText.length) && cutText.startsWith('\n\n') && cutText.endsWith('\n\n')) { return 2 }
+        if (cutText.startsWith('\n\n')) { return (cutText.endsWith('\n') && textBack.startsWith('\n')) ? 2 : 1 }
+        if (cutText.endsWith('\n\n')) { return (cutText.startsWith('\n') && textFront.endsWith('\n')) ? 2 : 1 }
+//        if (cutText.startsWith('\n\n')) { return (cutText.endsWith('\n') && textBack.startsWith('\n')) ? 2 : 1 }
+//        if (cutText.endsWith('\n\n')) { return (cutText.startsWith('\n') && textFront.endsWith('\n')) ? 2 : 1 }
+        if (cutText.startsWith('\n') && textFront.endsWith('\n')) { return 1 }
+        if (cutText.endsWith('\n') && textBack.startsWith('\n')) { return 1 }
+        return 0
+    }
+    static pasteBlocks(selectionStart, selectionEnd, pasteText, text, blocks) {
+        console.debug('pasteBlocks():', selectionStart, selectionEnd, pasteText, text, blocks)
+        const cutText = text.slice(selectionStart, selectionEnd)
+        const [index, _] = this.selected(selectionStart, selectionEnd, text)
+        console.debug(`index: ${index}`)
+        // ペースト位置のブロックと、その次のブロックの範囲のテキストを取得する。それらとペーストするテキストを結合してTextBlock.fromText()でブロック配列にして挿入する。これによって面倒な改行によるブロックの上書きや挿入といった個別判定が不要になる。
+        const textFront = text.slice(0, selectionStart)
+        const textBack = text.slice(selectionEnd)
+        console.debug('textFront:', textFront)
+        console.debug('textBack:', textBack)
+
+        console.debug('pasteText:', pasteText, (pasteText.match(/[\n]/g) || []).length)
+        const blockFront = textFront.slice((-1===textFront.lastIndexOf('\n\n')) ? 0 : textFront.lastIndexOf('\n\n')+2)
+        const blockBack = textBack.slice(0, (-1===textBack.indexOf('\n\n')) ? 0 : (0===textBack.indexOf('\n\n') ? textBack.slice(2).indexOf('\n\n')+2 : textBack.indexOf('\n\n')))
+        console.debug('blockFront:', blockFront)
+        console.debug('blockBack:', blockBack)
+        console.debug('blockBack.trimLine():', blockBack.trimLine())
+
+        const nextBlockText = (index+1<blocks.length) ? blocks[index+1] : ''
+        console.debug('nextBlockText:', nextBlockText)
+        console.debug('blockBack.trimLine()==nextBlockText:', blockBack.trimLine()==nextBlockText)
+        console.debug('(blockFront + pasteText + blockBack):', (blockFront + pasteText + blockBack))
+        console.debug('newBlockText:', (blockFront + pasteText + blockBack) + (((blockBack.trimLine()==nextBlockText) || (cutText.startsWith('\n') && cutText.endsWith('\n') && textFront.endsWith('\n') && textBack.startsWith('\n'))) ? '' : '\n\n' + nextBlockText))
+        console.debug('newBlockTextLines:', ((blockFront + pasteText + blockBack) + (((blockBack.trimLine()==nextBlockText) || (cutText.startsWith('\n') && cutText.endsWith('\n') && textFront.endsWith('\n') && textBack.startsWith('\n'))) ? '' : '\n\n' + nextBlockText)).split(/\r?\n/))
+        const pasteBlocks = TextBlock.fromLines(((blockFront + pasteText + blockBack) + (((blockBack.trimLine()==nextBlockText) || (cutText.startsWith('\n') && cutText.endsWith('\n') && textFront.endsWith('\n') && textBack.startsWith('\n'))) ? '' : '\n\n' + nextBlockText)).split(/\r?\n/))
+        console.debug('pasteBlocks:', pasteBlocks)
+        const cutInnerBlockCount = (cutText.trimLine().match(/[\n]{2,}/g) || []).length
+        const deleteCount = cutInnerBlockCount + 1 + this.#deleteCount(cutText, textFront, textBack)
+        console.debug(index, deleteCount, pasteBlocks)
+        return [index, pasteBlocks, deleteCount ]
+    }
+}
+window.TextBlock = TextBlock
+})()
